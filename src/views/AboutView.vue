2b99c747baeca3653490f529e88c87f16360f0d2
MemoIdeen <A01369297@tec.mx>
2023-04-06 23:08:31 -0500

    Final NLF

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,11 @@
 <template>
   <v-container>
-    <ReusableCard materia="Titulo de Materia" prof="Cr7"></ReusableCard>
+    <ReusableCard v-for="n in 7" :key="n"
+                  materia="Titulo de la materia"
+                  prof="Cristiano Ronaldo"
+                  classroom="La Congeladora"
+                  daysclass="Lunes, Martes & Miercoles"
+                  timeclass="7:00 - 9:00"></ReusableCard>
   </v-container>
 </template>
 <script setup>
